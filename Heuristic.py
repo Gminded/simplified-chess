4872ebb1410fff0dacb42fae79588f8b21cf589e
Gminded <gminded@gmail.com>
2015-07-12 18:41:27 +0200

    created heuristic

--- Heuristic.py
+++ Heuristic.py
@@ -1,5 +1,64 @@
+from ChessRules import ChessRules
+
 class Heuristic:
 
     @staticmethod
-    def HeuristicFunction(node):
-            node.utility = 2
+    def ShannonHeuristic(node, playerColor):
+        rules = ChessRules()
+
+        #Heuristic values
+        playerEnpassant = 0
+        adversaryEnpassant = 0
+        promotion = 0
+        checkMate = 0
+        playerKing = None
+        adversaryKing = None
+        playerPawns = []
+        adversaryPawns = []
+        playerMoves = 0
+        adversaryMoves = 0
+
+        #board state
+        board = node.GetState()
+        oldboard = node.GetOldState()
+
+        #checking possible states
+        if playerColor == "b":
+            adversaryColor = "w"
+        else:
+            adversaryColor = "b"
+        row_no = 0
+        col_no = 0
+        for row in board:
+            for column in row:
+                if column == playerColor+"P":
+                    playerPawns.append( (row_no, col_no) )
+                elif column == playerColor+"K":
+                    playerKing = ( row_no, col_no )
+                elif column == adversaryColor+"P":
+                    adversaryPawns.append( (row_no, col_no) )
+                elif column == adversaryColor+"K":
+                    adversaryKing = ( row_no, col_no )
+
+                col_no += 1
+            # advance row and clear column number
+            row_no += 1
+            col_no = 0
+
+        #Checking enpassant
+        for pawn in playerPawns:
+            if rules.IsEnpassantPawn(board, oldboard):
+                playerEnpassant += 1
+        for pawn in adversaryPawns:
+            if rules.IsEnpassantPawn(oldboard, board):
+                adversaryEnpassant += 1
+
+        #Checking
+
+
+import ChessBoard
+import ChessNode
+if __name__=="__main__":
+    board = ChessBoard.ChessBoard()
+    node = ChessNode.ChessNode(board.GetState(), board.GetState())
+    Heuristic.ShannonHeuristic(node, "b")
