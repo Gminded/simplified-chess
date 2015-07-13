53037d4423324710688c981d0b5d1c1d7e80ef9e
Antonio Cardace <ichigo663@gmail.com>
2015-07-13 15:18:37 +0200

    ZobristHashing skeleton

--- ZobristHash.py
+++ ZobristHash.py
@@ -1,4 +1,18 @@
 from random import *
 
 class ZobristHash:
-    pass
+
+    def __init__(self):
+        #costants
+        self.whitePawn = 1
+        self.whiteKing = 2
+        self.blackPawn = 3
+        self.blackKing = 4
+        self.randomBits = 32
+
+        #generate table
+        self.zobristTable = None
+        seed()
+        for i in range(64):
+            for j in range(4):
+                self.zobristTable[i][j] = getrandbits(self.randomBits)
