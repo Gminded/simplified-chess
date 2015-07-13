78ab22c87a5408441a58f4304ba582af330df1fc
Gminded <gminded@gmail.com>
2015-07-13 15:23:40 +0200

    hash function, incomplete

--- ZobristHash.py
+++ ZobristHash.py
@@ -1,4 +1,15 @@
 from random import *
+from ChessBoard import *
 
 class ZobristHash:
+    def hash(board):
+        state = board.state
+        h = 0
+        for row in range(8):
+            for col in range(8):
+                if state[row][col] != 'e':
+                    piece = self.pieceId[state[row][col]]
+                    h = h xor self.zobristTable[row*8+col][piece]
+        return h
+
     pass
